d5dfae71ad6903e0aeb7b57e253598a058f9dd97
Yannis JULIENNE <yjulienne@nuxeo.com>
2023-04-14 17:14:07 +0200

    NXCLI-78: Add LTS 2023 as default TP when Connect server is not reachable

--- utils/nuxeo-version-available.js
+++ utils/nuxeo-version-available.js
@@ -53,13 +53,13 @@ if (!res) {
     enabled: true
   }, {
     label: 'Nuxeo Platform LTS 2021',
-    default: true,
+    default: false,
     version: '2021.0',
     enabled: true
   }, {
-    label: 'Nuxeo Cloud 11.x',
+    label: 'Nuxeo Platform LTS 2023',
     default: true,
-    version: '11.1',
+    version: '2023.0',
     enabled: true
   }];
 }
@@ -94,9 +94,9 @@ let dtp = _(targetPlatforms).find((tp) => {
 
 if (dtp === undefined) {
   dtp = {
-    label: 'Nuxeo Platform LTS 2021',
+    label: 'Nuxeo Platform LTS 2023',
     default: true,
-    version: '2021.0'
+    version: '2023.0'
   };
 }
 
